821f8b53192eac87898970e98eecfa8e52e2270b
TanyeemSafwan <tanyeemsafwan123@gmail.com>
2023-10-16 07:05:42 +0600

    removed redundant code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,6 @@ export default defineNuxtConfig({
     enabled: true 
   },
   css: [
-    '~/assets/styles/main.css',
-    'bootstrap/dist/css/bootstrap.css'
+    '~/assets/styles/main.css'
   ]
 })
